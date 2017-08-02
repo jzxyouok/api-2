d211e0d323836a34f1d9d4adb6fde9900d747590
bing8u <676659348@qq.com>
2017-08-02 12:23:56 +0800

    update file

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         jeremykenedy\LaravelRoles\RolesServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -232,6 +233,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
