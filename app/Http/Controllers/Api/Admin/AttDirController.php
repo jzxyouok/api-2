ec1e76ff4b233700be961014e1db647f8918a6d9
bing8u <676659348@qq.com>
2017-08-21 13:02:41 +0800

    api小调整

--- app/Http/Controllers/Api/Admin/AttDirController.php
+++ app/Http/Controllers/Api/Admin/AttDirController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Admin;
 use App\AttDir;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class AttDirController extends Controller
@@ -17,9 +18,9 @@ class AttDirController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->except('is_sys');
+        $data = $request->all();
         Validator::make($data, [
-            'title' => 'required|between:3,15',
+            'title' => 'required|between:2,15',
             'parent_id' => 'integer',
         ], [], $this->attributes())->validate();
 
@@ -30,11 +31,12 @@ class AttDirController extends Controller
     {
         $data = $request->all();
         Validator::make($data, [
-            'title' => 'required_without_all:parent_id|between:3,15',
+            'title' => 'required_without_all:parent_id|between:2,15',
             'parent_id' => 'integer',
         ], [], $this->attributes())->validate();
 
-        return $attDir->update($data) ? 'success' : response('fail', 422);
+        $attDir->update($data);
+        return $attDir;
     }
 
     public function destroy(AttDir $attDir)
@@ -42,8 +44,12 @@ class AttDirController extends Controller
         if (count($attDir->children)) {
             return response('必须先删除子文件夹。', 422);
         }
-        if ($attDir->isSystem) {
-            return response('系统目录，不可删除。', 422);
+        // 删除附件
+        foreach ($attDir->attachments as $att) {
+            if (Storage::disk($att->disk)->exists($att->path)) {
+                Storage::disk($att->disk)->delete($att->path);
+            }
+            $att->delete();
         }
         return $attDir->delete() ? 'success' : response('delete folder fail', 422);
     }
